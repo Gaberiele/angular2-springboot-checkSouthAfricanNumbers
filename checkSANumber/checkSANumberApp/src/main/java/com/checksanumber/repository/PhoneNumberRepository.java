2e5708a3648aad6067d99f5fde2e24a39f4ac126
GabrieleCiardo <gabriele.ciardo@icsone.it>
2021-04-19 15:48:11 +0200

    clean repository

--- checkSANumber/checkSANumberApp/src/main/java/com/checksanumber/repository/PhoneNumberRepository.java
+++ checkSANumber/checkSANumberApp/src/main/java/com/checksanumber/repository/PhoneNumberRepository.java
@@ -3,16 +3,10 @@ package com.checksanumber.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import com.checksanumber.model.PhoneNumber;
 
 public interface PhoneNumberRepository extends JpaRepository<PhoneNumber, Long>{
 	
-	
-	@Query( name = "select n FROM PhoneNumber where n.tipoNum = ?1 order by n.id")
 	public List<PhoneNumber> findByTipoNum(String typeValidation);
-	
-	
 	
 }
